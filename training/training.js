400a7658aa98acccfa37430c1f55fe5d1b571d20
skyandy1891 <skyandy1891@gmail.com>
2022-05-10 21:53:48 +0300

    task-1 done

---
+++ training/training.js
@@ -0,0 +1,36 @@
+// Дан следующий объект с работниками и их зарплатами:
+// увеличьте зарплату всех работников на 10%
+
+// let obj = {
+//  employee1: 100,
+// 	employee2: 200,
+// 	employee3: 300,
+// 	employee4: 400,
+// 	employee5: 500,
+// 	employee6: 600,
+// 	employee7: 700,
+// };
+// const newObj = { ...obj };
+// let totalSalary = 0;
+// for (let key in newObj) {
+//     newObj[key] = newObj[key] * 1.1;
+//     totalSalary += newObj[key];
+// }
+// console.table(newObj);
+
+// ===============================
+// даны два массива :arr1 = [1, 2, 3, 4, 5] и arr2 = [6, 7, 8, 9, 10]
+// с помощью этих массивов создайте обьект, ключами которого будут элементы первого массива, а значениями элементы второго массива.
+
+// let arr1 = [1, 2, 3, 4, 5];
+// let arr2 = [6, 7, 8, 9, 10];
+// let obj = {};
+// for (let i = 0; i < arr1.length; i += 1) {
+//     let key = arr1[i],
+//      value = arr2[i];
+//     obj[key] = value;
+// }
+// console.log(obj);
+// =============================== play with markup ===============================
+
+
